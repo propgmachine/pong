abf72e55881ef392d994200d7f05416ce1a5ca79
propgmachine <77746698+propgmachine@users.noreply.github.com>
2021-03-31 22:42:48 -0400

    update

--- script.js
+++ script.js
@@ -2,31 +2,83 @@
 const GAME_AREA_WIDTH = 700;
 const GAME_AREA_HEIGHT = 500;
 
+// Size of the ball (in px)
+const BALL_SIZE = 20;
+
 // Size of the paddles (in px)
 const PADDLE_HEIGHT = 100;
 const PADDLE_WIDTH = 20;
 
-// Size of the ball (in px)
-const BALL_SIZE = 20;
+//Ball position and velocity
+let positionX = 100;
+let positionY = 100;
+let velocityX = 8;
+let velocityY = 0;
 
-// Get the computer paddle element
-const computerPaddle = document.querySelector('.computer-paddle');
+// Get the ball DOM element
+const ball = document.querySelector('.ball');
+
+
+// Computer paddle variables
+const computer = document.querySelector('.computer-paddle');
+let computerPositionY =100;
 
 // Initial computer paddle y-position and y-velocity
 let computerPaddleYPosition = 0;
 let computerPaddleYVelocity = 1;
 
+// Player paddle variables
+const player = document.querySelector('.player-paddle');
+let playerPositionY = 100;
+
+
+
 // Update the pong world
 function update() {
 
-    // Update the computer paddle's position
-    computerPaddleYPosition = computerPaddleYPosition + computerPaddleYVelocity;
+    positionX += velocityX;
+    positionY += velocityY;
 
-    // If the computer paddle goes off the edge of the screen, bring it back
-    computerPaddleYPosition = computerPaddleYPosition % (GAME_AREA_HEIGHT - PADDLE_HEIGHT);
+    computerPositionY = positionY;
 
-    // Apply the y-position 
-    computerPaddle.style.top = `${computerPaddleYPosition}px`;
+    // If we hit the bottom, make ball go in up direction
+    if (positionY >= GAME_AREA_HEIGHT - BALL_SIZE) {
+        velocityY = -velocityY;
+    }
+
+    // If the ball hits the left or the right, reset it
+    if (
+        positionX <= 0 ||
+        positionY >= GAME_AREA_WIDTH - BALL_SIZE
+    ) {
+        positionX = 100;
+        positionY = 100;
+    }
+
+    // If the ball hits the computer paddle, bounce it
+    if (
+        positionX >= GAME_AREA_WIDTH - BALL_SIZE - PADDLE_WIDTH &&
+        positionY >= computerPositionY - BALL_SIZE &&
+        positionY <= computerPositionY + PADDLE_HEIGHT
+    ) {
+        velocityX = -velocityX;
+    }
+
+    // If the ball hits the player paddle, bounce it
+    if (
+        positionX >= BALL_SIZE - PADDLE_WIDTH &&
+        positionY >= computerPositionY - BALL_SIZE &&
+        positionY <= computerPositionY + PADDLE_HEIGHT
+    ) {
+        velocityX = -velocityX;
+    }
+    
+    ball.style.top = '${positionY}px';
+    ball.style.left = '${positionX}px';
+
+    computer.style.top = '${computerPositionY}px';
+    player.style.top = '${playerPositionY}px';
+    
 }
 
 // Call the update() function everytime the browser is ready to re-render
